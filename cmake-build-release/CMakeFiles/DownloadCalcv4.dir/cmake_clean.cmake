96243aa2bafda135db48b447acffa426782abb04
NightSkyYT <lesage230012@sr.htlweiz.at>
2024-06-12 19:58:59 +0200

    Changed Color of the Program

---
+++ cmake-build-release/CMakeFiles/DownloadCalcv4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DownloadCalcv4.dir/main.c.obj"
+  "CMakeFiles/DownloadCalcv4.dir/main.c.obj.d"
+  "DownloadCalcv4.exe"
+  "DownloadCalcv4.exe.manifest"
+  "DownloadCalcv4.pdb"
+  "libDownloadCalcv4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DownloadCalcv4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
